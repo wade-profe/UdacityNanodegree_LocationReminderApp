75440ea39f16893f271db74a4bdc78330b006d51
wade-profe <wade.profe@gmail.com>
2023-09-20 10:13:56 +0200

    Changes

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -6,10 +6,16 @@ import com.udacity.project4.locationreminders.data.dto.Result
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
 
+    var shouldReturnError: Boolean = false
+    val remindersList: ArrayList<ReminderDTO> = arrayListOf()
+
 //    TODO: Create a fake data source to act as a double to the real data source
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error("Test exception")
+        }
+        return Result.Success(remindersList.toList())
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
@@ -23,6 +29,14 @@ class FakeDataSource : ReminderDataSource {
     override suspend fun deleteAllReminders() {
         TODO("delete all the reminders")
     }
+
+    fun addReminders(vararg reminder: ReminderDTO){
+        reminder.forEach {
+            remindersList.add(it)
+        }
+    }
+
+
 
 
 }
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,12 +1,38 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.koin.test.KoinTest
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
-class RemindersListViewModelTest {
+class RemindersListViewModelTest: KoinTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var reminderRepository: FakeDataSource
+
+    private lateinit var remindersListViewModel: RemindersListViewModel
+
+    @Before
+    fun setUp(){
+        reminderRepository = FakeDataSource()
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), reminderRepository)
+    }
+
+
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
 
