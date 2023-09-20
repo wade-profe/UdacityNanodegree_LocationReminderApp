eff0b0c3743f099eeab7a0f51cc72b76ff9a3483
wade-profe <wade.profe@gmail.com>
2023-09-20 14:02:06 +0200

    Changes

--- app/src/test/java/com/udacity/project4/locationreminders/MainCoroutineRule.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/MainCoroutineRule.kt
@@ -2,16 +2,18 @@ package com.udacity.project4.locationreminders
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()):
-    TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+class MainCoroutineRule(val dispatcher: TestDispatcher = UnconfinedTestDispatcher()):
+    TestWatcher() {
 
     override fun starting(description: Description?) {
         super.starting(description)
@@ -19,7 +21,6 @@ class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineD
 
     override fun finished(description: Description?) {
         super.finished(description)
-        cleanupTestCoroutines()
         Dispatchers.resetMain()
     }
 }
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -7,21 +7,27 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.MainCoroutineRule
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.ReminderDataSource
-import com.udacity.project4.locationreminders.data.local.LocalDB
-import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
-import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.locationreminders.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
-import org.koin.test.KoinTest
 import org.koin.test.get
+import org.koin.test.inject
+import org.hamcrest.Matchers.*
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
@@ -36,6 +42,7 @@ class RemindersListViewModelTest: AutoCloseKoinTest() {
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    val viewModel: RemindersListViewModel by inject()
 
     @Before
     fun init() {
@@ -60,6 +67,15 @@ class RemindersListViewModelTest: AutoCloseKoinTest() {
         runBlocking {
             repository.deleteAllReminders()
         }
+    }
+
+    @Test
+    fun loadReminders_showLoadingSetCorrectly() = runTest{
+        Dispatchers.setMain(StandardTestDispatcher())
+        viewModel.loadReminders()
+        assertThat(viewModel.showLoading.getOrAwaitValue(), equalTo(true))
+        advanceUntilIdle()
+        assertThat(viewModel.showLoading.getOrAwaitValue(), equalTo(false))
     }
 
 
